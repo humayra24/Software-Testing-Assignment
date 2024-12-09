f042c5f7e0e1f54ba4adb47f223ba5df90a92c13
xrg587 <humayra.noureen@tuni.fi>
2024-12-09 23:27:16 +0200

    Add coverage integration with Coveralls

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,9 +1,3 @@
-// export default {
-//   presets: ['@babel/preset-env'], // Ensures compatibility with older JavaScript versions
-//   plugins: ['@babel/plugin-transform-modules-commonjs'], // Transforms ES modules to CommonJS
-// };
-
-
 module.exports = {
   presets: ['@babel/preset-env'], // Use the environment preset for Babel
 };
